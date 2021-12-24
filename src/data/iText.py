bc2dd57c3e1dd4c8995cf22273566d247d56e261
Eric Misfeld <eric_misf@gmx.de>
2021-12-24 18:28:46 +0100

    changed iText to multiple Class File and util methods

--- src/data/iText.py
+++ src/data/iText.py
@@ -1,13 +1,49 @@
+def rotateText(text):
+    tmp = text[-1]
+    text = text[0:len(text)-1]
+    text = tmp + text
+    return text
+
+
 class Text:
     def __init__(self, text):
-        self.text = text
-        self.textArray = ()
+        self._text = text
 
     def getText(self):
-        return self.text
+        return self._text
+
+    def getTextLength(self):
+        return len(self._text)
 
     def charAt(self, index):
-        return self.text[index]
+        return self._text[index]
+
+    def getLastCahr(self):
+        return self._text[-1]
 
     def setText(self, text):
-        self.text = text
+        self._text = text
+
+
+class TextTable:
+    def __init__(self):
+        self.table = []
+        self.sortedTable = []
+
+    def addText(self, text):
+        self.table.append(text)
+
+    def getTextAtIndex(self, index):
+        return self.table[index]
+
+    def sortTable(self):
+        self.sortedTable = self.table.copy()
+
+    def getSortedTextAtIndex(self, index):
+        return self.sortedTable[index]
+
+    def getTextList(self):
+        return self.table
+
+    def printTable(self):
+        print(self.table)
--- src/data/iTextManipulator.py
+++
@@ -1,7 +0,0 @@
-class TextManipulator:
-
-    def rotateText(self, text):
-        tmp = text[-1]
-        text = text[0:len(text)-1]
-        text = tmp + text
-        return text
