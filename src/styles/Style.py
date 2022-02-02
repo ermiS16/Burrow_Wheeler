4f031e3cfd7552db255a4cf2a5c54ed63de6437c
Eric Misfeld <eric_misf@gmx.de>
2022-02-02 19:51:34 +0100

    added a style

--- src/styles/Style.py
+++ src/styles/Style.py
@@ -7,21 +7,35 @@ class Style(Enum):
     labelStyleSelected = 'labelStyleSelected'
     indexStyleMarked = 'indexStyleMarked'
     indexStyleSelected = 'indexStyleSelected'
+    indexStyleSimple = 'indexStyleSimple'
     labelDefaultStyle = 'labelDefaultStyle'
     descriptionStyle = 'descriptionStyle'
+    customLabelStyle = 'customLabelStyle'
 
 
 styles = {
     'labelStyle': r"background-color:red; color:white;",
-    'labelStyleCopyInit': "background-color:orange; color.white;",
+    'labelStyleCopyInit': "background-color:orange; color:white;",
     'labelStyleMarked': r"background-color:orange; color:white;",
     'labelStyleSelected': r"background-color:green; color:white;",
-    'indexStyleMarked': r"backgroud-color:gray; color:black;",
-    'indexStyleSelected': r"backgroud-color:green; color:white;",
+    'indexStyleMarked': r"background-color:gray; color:black;",
+    'indexStyleSelected': r"background-color:green; color:white;",
+    'indexStyleSimple': r"background-color:#eff0f1; color:black; border:1px solid black;",
     'labelDefaultStyle': r"background-color: #eff0f1; color:black;",
-    'descriptionStyle': r"border: 1px solid black;"
-
+    'descriptionStyle': r"border: 1px solid black;",
+    'customLabelStyle': r"background-color:{}; color:{};"
 }
 
 def getStyle(key):
+    print(str(styles.get(key.value)))
     return styles.get(key.value)
+
+def updateStyle(key, background, color):
+    style = getStyle(Style.labelDefaultStyle)
+
+def setBackgroundColor(color):
+    updateStyle("background", color)
+
+def setFontColor(color):
+    updateStyle("color", color)
+
