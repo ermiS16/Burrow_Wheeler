9414b77d9e120af2b4982f740dfe63f65302d86b
Eric Misfeld <eric_misf@gmx.de>
2022-02-16 19:37:56 +0100

    version update

--- src/setup.py
+++ src/setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 # from distutils.core import setup
 
 setup(
-    name='application',
-    version='0.1',
+    name='burrows-wheeler-transformation',
+    version='0.9',
     description="Gui Packages",
     author="Eric Misfeld",
     author_email="mier1011@h-ka.de",
