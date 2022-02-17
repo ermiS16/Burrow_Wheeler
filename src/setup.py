e3808169fa0b985ee1ae6c07d90bf782cfa20d9b
Eric Misfeld <eric_misf@gmx.de>
2022-02-17 20:28:25 +0100

    removed res from setup

--- src/setup.py
+++ src/setup.py
@@ -3,10 +3,9 @@ from setuptools import setup
 
 setup(
     name='burrows-wheeler-transformation',
-    version='0.9',
+    version='1.0',
     description="Gui Packages",
     author="Eric Misfeld",
     author_email="mier1011@h-ka.de",
-    packages=['app', 'controller', 'view', 'model', 'styles', 'res']
-    # package_dir = {'': 'src'}
+    packages=['app', 'controller', 'view', 'model', 'styles']
 )
