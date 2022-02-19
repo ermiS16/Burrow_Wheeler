0588020373639f1d42f975ffb8428a983b74d83d
Eric Misfeld <eric_misf@gmx.de>
2022-02-19 18:54:31 +0100

    renamed variable

--- src/controller/Speed.py
+++ src/controller/Speed.py
@@ -1,9 +1,9 @@
 class Speed:
     def __init__(self):
-        self._speedFactor = 1
+        self._speed_factor = 1
 
     def update(self, factor):
-        self._speedFactor = factor
+        self._speed_factor = factor
 
     def getFactor(self):
-        return self._speedFactor
+        return self._speed_factor
